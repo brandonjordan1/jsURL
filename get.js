810b7f692993a42b7e060d7a974ad7d725b7d557
Brandon Jordan <brandon_jordan360@yahoo.com>
2020-09-02 13:15:04 -0400

    Update get.js

--- get.js
+++ get.js
@@ -1,16 +1,23 @@
 function get(e) {
-  var r = window.location.href.replace(new RegExp("\\+", "g"), "%20").split("?");
-  if (0 == r.length) return "";
-  if (r[1]) {
-    var n = r[1].split("&"),
-      t = "";
-    for (i = 0; i < n.length; i++) {
-      var a = n[i].split("=");
-      if (a[0] == e) {
-        t = a[1];
-        break
+  if (!/MSIE|Trident/.test(window.navigator.userAgent)) { // if not IE
+    var url = new URL(window.location.href);
+    var value = url.searchParams.get(e);
+    if(value)return value;
+    else return;
+  } else {
+    var r = window.location.href.replace(new RegExp("\\+", "g"), "%20").split("?");
+    if (0 == r.length) return "";
+    if (r[1]) {
+      var n = r[1].split("&"),
+          t = "";
+      for (i = 0; i < n.length; i++) {
+        var a = n[i].split("=");
+        if (a[0] == e) {
+          t = a[1];
+          break
+        }
       }
-    }
-    return (t = unescape(t)).replace("+", " "), t
-  } else return;
+      return (t = unescape(t)).replace("+", " "), t
+    } else return;
+  }
 }
